f8bd25160458c3c4a69f96c9b1de6fc6e59454eb
Oleg K <voknevark@gmail.com>
2020-09-05 09:20:39 -0700

    Update Helper\Data.php

--- app/code/Hapex/SessionCleanup/Helper/Data.php
+++ app/code/Hapex/SessionCleanup/Helper/Data.php
@@ -3,18 +3,12 @@
 namespace Hapex\SessionCleanup\Helper;
 
 use Hapex\Core\Helper\DataHelper;
-use Magento\Framework\App\Helper\Context;
-use Magento\Framework\ObjectManagerInterface;
 
 class Data extends DataHelper
 {
     protected const XML_PATH_CONFIG_ENABLED = "hapex_sessioncleanup/general/enable";
     protected const XML_PATH_CONFIG_COOKIE_LIFETIME = "web/cookie/cookie_lifetime";
     protected const FILE_PATH_LOG = "hapex_session_cleanup";
-    public function __construct(Context $context, ObjectManagerInterface $objectManager)
-    {
-        parent::__construct($context, $objectManager);
-    }
 
     public function isEnabled()
     {
