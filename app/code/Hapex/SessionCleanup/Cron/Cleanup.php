92176c82b352104cc0806d566ae1fd6a5a74a970
Oleg K <voknevark@gmail.com>
2020-09-21 11:57:27 -0700

    Update Cron\Cleanup.php

--- app/code/Hapex/SessionCleanup/Cron/Cleanup.php
+++ app/code/Hapex/SessionCleanup/Cron/Cleanup.php
@@ -9,12 +9,14 @@ use Hapex\SessionCleanup\Helper\Data as DataHelper;
 class Cleanup extends BaseCron
 {
     protected $resource;
-    private $tableSession;
+    protected $connection;
+    protected $tableSession;
 
     public function __construct(DataHelper $helperData, LogHelper $helperLog, ResourceConnection $resource)
     {
         parent::__construct($helperData, $helperLog);
         $this->resource = $resource;
+        $this->connection = $this->resource->getConnection();
         $this->tableSession = $this->resource->getTableName("session");
     }
 
@@ -26,7 +28,6 @@ class Cleanup extends BaseCron
                     $this->helperData->log("");
                     $this->helperData->log("Starting Session Cleanup");
                     $this->deleteSessions();
-                    $this->optimizeSessionTable();
                     $this->helperData->log("Ending Session Cleanup");
                 } catch (\Exception $e) {
                     $this->helperData->errorLog(__METHOD__, $e->getMessage());
@@ -36,31 +37,17 @@ class Cleanup extends BaseCron
         }
     }
 
-    private function deleteSessions()
+    protected function deleteSessions()
     {
         try {
-            $connection = $this->resource->getConnection();
             $table = $this->tableSession;
             $cookieLifetime = $this->helperData->getCookieLifetime();
             $cookieLifetime = !empty($cookieLifetime) ? $cookieLifetime : 3600;
             $this->helperData->log("- Looking for sessions expired within $cookieLifetime seconds");
             $sql = "DELETE FROM $table WHERE session_expires < UNIX_TIMESTAMP() - $cookieLifetime;";
-            $result = $connection->query($sql);
+            $result = $this->connection->query($sql);
             $count = $result->rowCount();
             $this->helperData->log("- Cleaned $count expired sessions");
-        } catch (\Exception $e) {
-            $this->helperData->errorLog(__METHOD__, $e->getMessage());
-        }
-    }
-    private function optimizeSessionTable()
-    {
-        try {
-            $connection = $this->resource->getConnection();
-            $table = $this->tableSession;
-            $sql = "OPTIMIZE TABLE $table;";
-            $this->helperData->log("- Optimizing sessions table");
-            $connection->query($sql);
-            $this->helperData->log("- Sessions table optimized");
         } catch (\Exception $e) {
             $this->helperData->errorLog(__METHOD__, $e->getMessage());
         }
